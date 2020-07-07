2b97724a66bb0f9f35135e4e5c5de5e3609ff286
Matthew Wilson <matthew.wilson@uk.ibm.com>
2020-07-07 13:45:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mpReactive Messaging and EJBs
+
+Recreate to show that mpReactiveMessaging can't be used with an EJB.  
+
+To run: `mvn liberty:dev`
---
+++ src/main/java/dev/appsody/starter/StarterApplication.java
@@ -0,0 +1,43 @@
+package dev.appsody.starter;
+
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import javax.ejb.Stateless;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Stateless
+public class StarterApplication {
+    private static Logger logger = LoggerFactory.getLogger(StarterApplication.class);
+    private String receivedMessage;
+
+    /**
+     * Process an event from one topic and emit a result to another.
+     * @param message
+     * @return
+     */
+    @Incoming("incomingTopic1")
+    @Outgoing("outgoingTopic1")
+    public String processMessage(String message) {
+        String completeMessage = "Hello " + message;
+        logger.info("Message " + completeMessage);
+        return completeMessage;
+    }
+
+    /**
+     * Receive an event. This could be used to do something else with the data
+     * such as writing it to a database.
+     * @param message
+     */
+    @Incoming("incomingTopic2")
+    public void receiveMessage(String message) {
+        receivedMessage = message;
+        logger.info("Message " + message);
+    }
+
+	public String getReceivedMessage() {
+		return receivedMessage;
+	}
+}
